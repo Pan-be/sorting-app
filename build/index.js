3ae96947db5e14fab26dc4fbb8ca971eaf4fac99
Konrad <konradbieniek@gmail.com>
2023-03-15 22:22:18 +0100

    initial commit

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+class SortData {
+    constructor(data) {
+        this.data = data;
+    }
+    sort() { }
+}
+const dateArray = [
+    new Date(2010, 1, 12),
+    new Date(2009, 6, 6),
+    new Date(2019, 8, 31),
+    new Date(2023, 3, 23),
+];
+const years = dateArray.map((date) => date.getFullYear());
+const sortData = new SortData(years);
+sortData.sort();
+console.log(sortData.data);
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+class SortData {
+	constructor(public data: number[]) {}
+
+	sort(): void {}
+}
+
+const dateArray = [
+	new Date(2010, 1, 12),
+	new Date(2009, 6, 6),
+	new Date(2019, 8, 31),
+	new Date(2023, 3, 23),
+]
+
+const years = dateArray.map((date) => date.getFullYear())
+
+const sortData = new SortData(years)
+
+sortData.sort()
+
+console.log(sortData.data)
